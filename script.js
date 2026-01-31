dce4f4ee8b0aa50112dc090e5455d273c9fe1fa1
titus882 <kipsnetfilx@gmail.com>
2026-01-31 18:27:23 +0300

    Add why scene with reason saving and page persistence

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 angular.module('valentineApp',[])
 .controller('MainCtrl', ['$scope','$timeout', function($scope,$timeout){
-  // scene can be 'landing' or 'question'
-  $scope.scene = 'landing';
+  // Initialize scene from localStorage or default to 'landing'
+  $scope.scene = localStorage.getItem('currentScene') || 'landing';
   $scope.message = '';
   $scope.accepted = false;
+  $scope.whyReason = '';
+  $scope.reasons = JSON.parse(localStorage.getItem('reasons')) || [];
+
+  // Save scene to localStorage whenever it changes
+  $scope.$watch('scene', function(newScene){
+    if(newScene) {
+      localStorage.setItem('currentScene', newScene);
+    }
+  });
 
   $scope.openGift = function(){
     $scope.scene = 'question';
@@ -15,19 +24,35 @@ angular.module('valentineApp',[])
     $scope.message = '';
     if(s === 'landing'){
       $scope.accepted = false;
+      localStorage.removeItem('reasons');
+      $scope.reasons = [];
     }
   };
 
   $scope.accept = function(){
     $scope.accepted = true;
     $scope.message = '';
-    $scope.scene = 'gift-choice';
+    $scope.scene = 'why';
+  };
+
+  $scope.saveReason = function(){
+    if($scope.whyReason.trim()){
+      $scope.reasons.push($scope.whyReason);
+      localStorage.setItem('reasons', JSON.stringify($scope.reasons));
+      $scope.whyReason = '';
+      $timeout(function(){
+        $scope.scene = 'gift-choice';
+      }, 500);
+    }
   };
 
   $scope.selectGift = function(giftType){
     if(giftType === 'photo'){
-      // navigate to photo gallery
       $scope.scene = 'photo-gallery';
+    } else if(giftType === 'letter'){
+      $scope.scene = 'love-letter';
+    } else if(giftType === 'voucher'){
+      $scope.scene = 'voucher';
     } else {
       $scope.message = 'Thank you! 💕';
       $timeout(function(){
